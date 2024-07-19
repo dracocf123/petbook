3054fad9d83d4c9d01ee174680e1839e42bbb782
Jay Anthony Gonzaga <jayanthonygonzaga123@gmail.com>
2024-07-19 12:58:58 +0800

    asdasdasda

--- index.php
+++ index.php
@@ -268,7 +268,7 @@
                 <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h2>Planning to Adopt a Cat?</h2>
+                      <p>Planning to Adopt a Dog?</p>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
